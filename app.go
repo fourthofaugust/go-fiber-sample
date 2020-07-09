605a13ec76ed47875cfd5bb5b42d6191a134f164
fourthofaugust <fourthofaugust@users.noreply.github.com>
2020-07-09 03:19:06 -0400

    Initial commit

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Send("Welcome Fourth of August")
+	})
+
+	app.Listen(3001)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/fourthofaugust/go-fiber-sample
+
+go 1.14
+
+require (
+	github.com/gofiber/fiber v1.12.5 // indirect
+	github.com/klauspost/compress v1.10.10 // indirect
+)
